0d3aad2ad402be5a504b0060a0aa46c15a3f358c
Kaan Ersoy <kaanersoy12@gmail.com>
2021-01-08 22:54:40 +0300

    Deploy heroku

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get('/404', (req,res) => {
+    res.redirect('404.html');
+})
+
 app.get('/:id', async (req,res) => {
     const { id:slug } = req.params;
     try {
@@ -29,11 +33,10 @@ app.get('/:id', async (req,res) => {
         if(redirectUrl){
             res.redirect(redirectUrl.url);
         }
-        res.redirect(`?error=${slug} is not found`);
+        res.redirect('404.html');
     } catch (err) {
-        res.redirect(`?error=Link is not found`);
+        res.redirect('404.html');
     }
-    
 })
 
 const schema = yup.object().shape({
@@ -44,19 +47,20 @@ const schema = yup.object().shape({
 app.post('/url', async (req,res,next) =>{
     let {slug, url} = req.body;
     try {
+        if(!slug){
+            slug = nanoid(6);
+        }
+
         await schema.validate({
             slug,
             url
         })
-        if(!slug){
-            slug = nanoid(6);
-        } else {
-            const isExist = await urls.findOne({ slug });
-            if(isExist) {
-                throw new Error('Slug is in use. 🤦‍♂️');
-            }
-        }
+
         slug = slug.toLowerCase();
+        const isExist = await urls.findOne({ slug });
+        if(isExist) {
+            throw new Error('Slug is in use. 🤦‍♂️');
+        }
         const newUrl = {
             url,
             slug,
@@ -68,18 +72,11 @@ app.post('/url', async (req,res,next) =>{
     }
 })
 
-app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "script-src 'self' https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js")
-    next();
-})
-  
-
 app.use((error, req, res, next) => {
     if(error.status){
         res.status(error.status);
     } else {
         res.status(500);
-        
     }
     res.json({
         message: error.message,
@@ -87,7 +84,7 @@ app.use((error, req, res, next) => {
     })
 });
 
-const port = process.env.PORT || 5000;
+const port = PORT || 5000;
 app.listen(port, () => {
     console.log(`Port is active in ${port}`);
 })
--- public/app.js
+++ public/app.js
@@ -3,20 +3,45 @@ const app = new Vue({
     data: {
         url: null,
         slug: null,
-        isCreated: false
+        isActive: false,
+        responseMessage: null,
+        isLinkUsable: false,
+        redirectURL: null
     },
     methods:{
-        getShortUrl: async () => {
-            const data = {
-                url,
-                slug
+        getShortUrl: async function(){
+            var formData = null;
+            if(this.slug != ""){
+                formData = {url:this.url, slug:this.slug};
+            }else{
+                formData = {url:this.url}
             }
             const response = await fetch('/url', {
                 method: 'POST',
                 headers: {'Content-type' : 'application/json'},
-                body: JSON.stringify(data)
+                body: JSON.stringify(formData)
             })
-            return response.json();
+            .then(response => response.json())
+            .then(data => {
+                this.isActive=true;
+                this.responseMessage=data.message;
+                if(data.url){
+                    if(this.slug != "" || !this.slug || this.slug == null){
+                        this.slug = data.slug;
+                    }
+                    this.isLinkUsable = true;
+                    this.redirectURL = `${window.location.origin}/${this.slug}`
+                    this.responseMessage = "Link is Created😍"
+                    
+                }
+            })
+            .catch((err) => {
+                console.error("Error : ", err);
+            })
+        },
+        goBack: function(){
+            this.isActive=false;
+            console.log(this.isActive);
         }
     }
 })
