0de39e0d50728c09124d5af5cb56ffc4a51489d7
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-24 10:59:15 +0300

    Change method for create same url slug issue

--- api/controllers/url.js
+++ api/controllers/url.js
@@ -11,30 +11,36 @@ urls.createIndex('slug');
 const getUrlById = ash(async (req, res) => {
     const { id:slug } = req.params;
 
-    const redirectUrl = await urls.findOne( {slug });
+    const redirectUrl = await urls.findOne({ slug });
 
     res.redirect(redirectUrl.url || '404.html');
 });
 
-const createUrl = ash(async (req, res, next) => {
-    let { slug, url } = req.body;
+const createUrl = ash(async (req, res) => {
+  let { slug, url } = req.body;
+  
+  // Checks url is exists or additional slug sended
+  const isUrlExist = await urls.findOne({ url });
+  if(!slug && isUrlExist){
+    return res.json(isUrlExist)
+  }
 
-    slug = slug || nanoid(5);
-    
-    await urlSchema.validate({ slug, url });
+  // if slug not sended with request randomizes it!
+  slug = slug || nanoid(6)
 
-    slug = slug.toLowerCase();
-    
-    const isUrlExist = await urls.findOne({ slug });
+  // Validate the schema
+  await urlSchema.validate({ slug, url });
+  
+  // if slug in use throws error.
+  const isSlugExist = await urls.findOne({ slug });
+  if (isSlugExist) {
+    throw new Error('Slug is in use. 🤦‍♂️');
+  }
 
-    if (isUrlExist) {
-        throw new Error('Slug is in use. 🤦‍♂️');
-    }
+  const newUrl = { url, slug };
 
-    const newUrl = { url, slug };
-
-    const createdUrl = await urls.insert(newUrl);
-    res.json(createdUrl);
+  const createdUrl = await urls.insert(newUrl);
+  res.json(createdUrl);
 });
 
 module.exports = { getUrlById, createUrl };
